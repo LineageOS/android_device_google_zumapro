cebd70e7599053ee1a6aa24b559cdb3f14e2dbf1
Michael Bestas <mkbestas@lineageos.org>
2026-03-04 05:11:57 +0200

    zumapro: Import common lineage overlays from devices
    Change-Id: I415877cf0705dae241b425c71cfa54d504cce7c6

--- device.mk
+++ device.mk
@@ -756,6 +756,10 @@ PRODUCT_PACKAGES += \
     TelecomOverlayProductZumapro \
     TelephonyProviderOverlayProductZumapro
 
+PRODUCT_PACKAGES += \
+    LineageSdkOverlayZumapro \
+    SettingsOverlayZumapro
+
 # Parts
 PRODUCT_PACKAGES += \
     GoogleParts
